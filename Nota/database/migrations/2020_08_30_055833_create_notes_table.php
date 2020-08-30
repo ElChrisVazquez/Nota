287ef88fc373a527b0a6f44e5cf6cf1b2c60148e
ElChrisVazquez <TheChrisVazquez@gmail.com>
2020-08-30 01:17:45 -0500

    se crea migracion de notas y clientes

--- Nota/database/migrations/2020_08_30_055833_create_notes_table.php
+++ Nota/database/migrations/2020_08_30_055833_create_notes_table.php
@@ -15,7 +15,7 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('cliente');
         });
     }
 
---
+++ Nota/database/migrations/2020_08_30_060532_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('paterno');
+            $table->string('materno');
+            $table->string('calle');
+            $table->string('exterior');
+            $table->string('interior');
+            $table->string('cp');
+            $table->string('ciudad');
+            $table->string('estado');
+            $table->string('pais');
+            $table->string('telefono');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
